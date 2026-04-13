09038352a96e84fa1619100cf9cce4963efe054e
danila.golovenko <dgolovenko01@gmail.com>
2026-04-13 22:46:13 +0400

    rogue item chest

--- Source/Course/World/RogueItemChest.cpp
+++ Source/Course/World/RogueItemChest.cpp
@@ -9,18 +9,33 @@ ARogueItemChest::ARogueItemChest()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	
+	BaseMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMeshComp"));
+	RootComponent = BaseMeshComp;
+	
+	LidMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("LidMeshComp");
+	LidMeshComp->SetupAttachment(BaseMeshComp);
 }
 
 // Called when the game starts or when spawned
 void ARogueItemChest::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	SetActorTickEnabled(true);
 }
 
 // Called every frame
 void ARogueItemChest::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	CurrentAnimationPitch = FMath::FInterpConstantTo(CurrentAnimationPitch, AnimationTargetPitch, DeltaTime, AnimationSpeed);
+	LidMeshComp->SetRelativeRotation(FRotator(CurrentAnimationPitch, 0.f, 0.f));
+	
+	if (FMath::IsNearlyEqual(CurrentAnimationPitch, AnimationTargetPitch))
+	{
+		SetActorTickEnabled(false);
+	}
 }
 
--- Source/Course/World/RogueItemChest.h
+++ Source/Course/World/RogueItemChest.h
@@ -14,8 +14,21 @@ class COURSE_API ARogueItemChest : public AActor
 public:
 	// Sets default values for this actor's properties
 	ARogueItemChest();
-
+	
 protected:
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<UStaticMeshComponent> BaseMeshComp;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<UStaticMeshComponent> LidMeshComp;
+
+	UPROPERTY(EditDefaultsOnly, Category="Animation")
+	float AnimationSpeed = 50.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Animation")
+	float AnimationTargetPitch = 120.f;
+	
+	float CurrentAnimationPitch = 0.0f;
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
